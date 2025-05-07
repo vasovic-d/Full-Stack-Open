813ab4c1fd33730675582ad2c9a4250c50ead6af
vasovic-d <vasvicdhm@gmail.com>
2025-05-07 21:51:53 +0200

    Final courseinfo 2

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,34 @@
+const Course = ({ course }) => {
+    return (
+      <div>
+        <Header name = {course.name}/>
+        <Content parts = {course.parts}/>
+        <Total  parts = {course.parts}/>
+      </div>
+    )
+}
+  
+const Header = ({ name }) => <h1>{name}</h1>
+  
+const Content = ({ parts }) => {
+    return (
+        <div>
+            {parts.map(part => <Part key = {part.id} part = {part}/>)}
+        </div>
+    )
+}
+  
+const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+    return (
+    <p>
+      <b>
+        Total of {total} exercises
+      </b>
+    </p>
+    )
+}
+  
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+
+export default Course
