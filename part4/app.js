d461d91d3a75d5d0bea6559703e7c09b3c5153ab
vasovic-d <vasvicdhm@gmail.com>
2025-05-23 01:55:15 +0200

    Blog list step 1-2

---
+++ part4/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+module.exports = app
